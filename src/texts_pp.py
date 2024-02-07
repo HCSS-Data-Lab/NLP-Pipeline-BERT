43c68cba2666d0e21e03ae0721a23b9336633dba
arneeichholtz <arne.eichholtz@hotmail.com>
2024-02-07 15:37:50 +0100

    Better documantation text_pp

--- src/texts_pp.py
+++ src/texts_pp.py
@@ -3,15 +3,30 @@ from tqdm import tqdm
 import spacy
 import pickle
 
-import config
+def chunk_texts(texts, chunk_size=1000):
+    """
+    Chunks texts into segments of specified chunk size.
+
+    Args:
+        texts (list[str]): input texts
+        chunk_size (int): The length of each chunked text segment.
+
+    Returns:
+        list[str]: A list with the chunked text segments.
+    """
+    chunks_out = []
+    for text in texts:
+        chunks = make_chunks(text, chunk_size)
+        chunks_out.extend(chunks)
+    return chunks_out
 
 def make_chunks(text, max_length):
     """
     Chunk individual text to pieces of max length
 
     Args:
-        text (str):
-        max_length (int):
+        text (str)
+        max_length (int)
 
     Returns:
         chunks (list[str]): text in chunks
@@ -22,35 +37,6 @@ def make_chunks(text, max_length):
         chunks.append(chunk)
         text = text[max_length:]
     return chunks
-
-def pair_sentences(sentences):
-    """
-    Pair sentences together
-
-    Args:
-        sentences (list[str]):
-
-    Returns:
-        list[str]: paired sentences
-    """
-    return [(sentences[i]+""+sentences[i+1] if i+1 < len(sentences) else sentences[i]) for i in range(0, len(sentences), 2)]
-
-def chunk_texts(texts, chunk_size=1000):
-    """
-    Chunks texts into segments of specified chunk size.
-
-    Args:
-        texts (list[str]): input texts
-        chunk_size (int): The length of each chunked text segment.
-
-    Returns:
-        list[str]: A list containing the chunked text segments.
-    """
-    chunks_out = []
-    for text in texts:
-        chunks = make_chunks(text, chunk_size)
-        chunks_out.extend(chunks)
-    return chunks_out
 
 def sentencize_text(texts):
     """
@@ -67,15 +53,27 @@ def sentencize_text(texts):
     """
     return [sentence for t in texts for sentence in t.split(".")]
 
+def pair_sentences(sentences):
+    """
+    Pair sentences together
+
+    Args:
+        sentences (list[str])
+
+    Returns:
+        list[str]: paired sentences
+    """
+    return [(sentences[i]+""+sentences[i+1] if i+1 < len(sentences) else sentences[i]) for i in range(0, len(sentences), 2)]
+
 def filter_texts(texts):
     """
-    Filter text on stopwords and punctuation using Spacy, save lemmatized words
+    Filter text on stopwords and punctuation using Spacy, save lemmas of words
 
     Args:
         texts (list[str]): input texts
 
     Returns:
-        lemmas (list[str]): filtered text
+        lemmas (list[str]): filtered, lemmatized text
     """
     print("Filtering texts...")
     nlp = spacy.load('en_core_web_lg', disable=['parser', 'ner'])
@@ -87,7 +85,7 @@ def filter_texts(texts):
 
 class TextPreProcess:
 
-    def __init__(self, splits_from_file, text_bodies_path, split_texts_path, clean_meth, split_size):
+    def __init__(self, splits_from_file, text_bodies_path, splits_path, clean_meth, split_size):
         """
         Class TextPreProcess stores the variables needed in text preprocessing: splits from file,
         text-bodies path, split-texts path, text clean method and text split size. It also handles
@@ -97,9 +95,10 @@ class TextPreProcess:
         Parameters:
             splits_from_file (bool): Load split texts from file or read and split text bodies at runtime
             text_bodies_path (str): Directory path where text bodies are stored.
-            split_texts_path (str): Directory path where split texts are stored.
-            clean_meth (str): Text clean method.
-            split_size (str): Text split size.
+            splits_path (str): Directory path where split texts are stored.
+            clean_meth (str): Text clean method. (def for default, ft for filter-text function,
+                              vect for vectorization param in BERTopic)
+            split_size (str): Text split size. (chunk, sentence, or sentence-pairs)
 
         Attributes:
             texts_split_name (str): text split .pkl dictionary filename
@@ -107,13 +106,20 @@ class TextPreProcess:
 
         self.splits_from_file = splits_from_file
         self.bodies_path = text_bodies_path
-        self.split_texts_path = split_texts_path
+        self.splits_path = splits_path
 
         self.clean_meth = clean_meth
         self.split_size = split_size
         self.texts_split_name = f"texts_{self.split_size}_{self.clean_meth}.pkl"
 
     def get_texts(self):
+        """
+        Get text data calls loads_split_texts() to load split texts from file or
+        generate_split_texts() to read text bodies and split them at runtime.
+
+        Returns:
+            texts (list[str]): split text data
+        """
         if self.splits_from_file:
             texts = self.load_split_texts()
         else:
@@ -123,7 +129,14 @@ class TextPreProcess:
         return texts
 
     def load_split_texts(self):
-        path = os.path.join(self.split_texts_path, self.texts_split_name)
+        """
+        Load split texts stored as .pkl dict from self.splits_path
+
+        Returns:
+            list[str]: split texts from file
+
+        """
+        path = os.path.join(self.splits_path, self.texts_split_name)
         if os.path.exists(path):
             print(f"Split texts file name: {self.texts_split_name}. Reading split text elements from file...")
             with open(path, "rb") as file:
@@ -135,13 +148,23 @@ class TextPreProcess:
                 f"Generate it at runtime.")
 
     def generate_split_texts(self):
+        """
+        Generate split texts at runtime by reading text bodies from folder with read_input_texts() and
+        splitting them in split_size pieces with split_texts(). If clean_meth = "ft", split texts are
+        cleaned by function filter_text(). Split texts are returned and stored as .pkl dict in splits_path.
+
+        Returns:
+            split_texts (list[str]): list with split texts, so each entry is chunk, sentence or sentence-pair
+                                     depending on split_size attribute.
+
+        """
         print("Read text bodies and splitting at runtime...")
         if os.listdir(self.bodies_path):  # Empty folder evaluates to 0
             text_bodies = self.read_input_texts()
             split_texts = self.split_texts(text_bodies)
 
-            with open(os.path.join(self.split_texts_path, self.texts_split_name), "wb") as file:
-                pickle.dump({'texts': split_texts}, file, protocol=pickle.HIGHEST_PROTOCOL)
+            with open(os.path.join(self.splits_path, self.texts_split_name), "wb") as file:
+                pickle.dump({'texts': split_texts, 'text_split_size': self.split_size}, file, protocol=pickle.HIGHEST_PROTOCOL)
 
             return split_texts
         else:
@@ -149,7 +172,11 @@ class TextPreProcess:
 
     def read_input_texts(self):
         """
-        Read text bodies saved as .txt files in text_bodies_path,
+        Read input text bodies saved as .txt from bodies_path folder.
+
+        Returns:
+            texts (lst[str]): text bodies
+
         """
         text_names = sorted([text_file for text_file in os.listdir(self.bodies_path) if text_file.endswith('.txt')])
         print(f'{"Number of texts in folder:":<65}{len(text_names):>10}')
@@ -163,7 +190,15 @@ class TextPreProcess:
 
     def split_texts(self, texts):
         """
-        Split text bodies into split size and apply filter function if clean_meth = "ft" (filter_texts)
+        Split input text bodies input split_size pieces and filter split texts with
+        function filter_texts() if clean_meth = "ft".
+
+        Args:
+            texts (list[str]): input text bodies
+
+        Returns:
+            list[str]: split (and filtered) text pieces
+
         """
         if self.split_size == "chunk":
             splits = chunk_texts(texts)
