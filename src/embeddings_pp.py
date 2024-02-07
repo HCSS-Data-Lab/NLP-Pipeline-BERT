77c1e5be486d53e80a1c997e31d62d222deb8ac2
arneeichholtz <arne.eichholtz@hotmail.com>
2024-02-07 16:10:08 +0100

    Update doc for emb_pp

--- src/embeddings_pp.py
+++ src/embeddings_pp.py
@@ -8,18 +8,22 @@ class EmbeddingsPreProcess:
 
     def __init__(self, emb_from_file, emb_path, clean_meth, split_size):
         """
-        Initializes the EmbeddingsPreProcess object with necessary configurations.
+        Class EmbeddingsPreProcess stores the variables needed to initialize the embeddings: emb_from_file,
+        embeddings path, text clean-method, and text split-size. It also handles the functionality to
+        initialize embeddings: loading pre-trained embeddings from file or generating them at runtime
+        based on input text data.
 
         Parameters:
-            emb_path (str): Path where embeddings are stored or will be saved.
-            clean_method (str): Method used for text cleaning.
-            split_size (str): Size of text chunks for splitting before embedding.
+            emb_from_file (bool): Boolean indicator whether to load existing embeddings from file or generate them
+                                  at runtime from text data.
+            emb_path (str): Directory path where embeddings are stored.
+            clean_meth (str): Text clean method. (def for default, ft for filter-text function,
+                              vect for vectorization param in BERTopic)
+            split_size (str): Text split size. (chunk, sentence, or sentence-pairs)
 
         Attributes:
-            emb_path (str):
-            clean_method (str):
-            split_size (str):
-            embedding_name (str): embeddings filename based on split_size and clean_method
+            embedding_name (str): embedding filename based on split_size and clean_method.
+            bert_model (str): pre-trained sentence BERT model name, defined in config.
         """
 
         self.emb_from_file = emb_from_file
@@ -30,6 +34,16 @@ class EmbeddingsPreProcess:
         self.bert_model = config.emb_pp_params["bert_model"]
 
     def get_embeddings(self, data):
+        """
+        Get embeddings by loading them from file with load_embeddings() or generate
+        them at runtime based on input text data with generate_embeddings().
+
+        Args:
+            data (list[str]): input text data
+
+        Returns:
+            embeddings (torch.Tensor): text embeddings, each doc as a 768-dim vector. Shape: (num docs, 768)
+        """
         if self.emb_from_file:
             embeddings = self.load_embeddings()
         else:
@@ -39,7 +53,10 @@ class EmbeddingsPreProcess:
 
     def load_embeddings(self):
         """
-        Loads embeddings from file.
+        Load embeddings saved as .pkl dict file saved in folder self.path.
+
+        Returns:
+            embeddings (torch.Tensor): text embeddings, each doc as a 768-dim vector. Shape: (num docs, 768)
         """
         path = os.path.join(self.path, self.embedding_name)
         if os.path.exists(path):
@@ -54,19 +71,22 @@ class EmbeddingsPreProcess:
 
     def generate_embeddings(self, data):
         """
-        Generates embeddings at runtime using a pre-defined model.
+        Generate embeddings from input text data using SentenceTransformer module.
+        Return embeddings and save in folder self.path the embeddings, text_split_size,
+        and bert_model as .pkl dict. Sentence BERT model name given by self.bert_model.
+
+        Args:
+            data (list[str]): input text data
+
+        Returns:
+            embeddings (torch.Tensor): text embeddings, each doc as a 768-dim vector. Shape: (num docs, 768)
         """
         print("Initializing embeddings at runtime...")
         model = SentenceTransformer(self.bert_model)
         embeddings = model.encode(data, show_progress_bar=True)
 
         with open(os.path.join(self.path, self.embedding_name), "wb") as file:
-            pickle.dump({'embeddings': embeddings, 'embedding-text-size': self.split_size, 'bert_model': self.bert_model}, file,
+            pickle.dump({'embeddings': embeddings, 'text_split_size': self.split_size, 'bert_model': self.bert_model}, file,
                         protocol=pickle.HIGHEST_PROTOCOL)
         return embeddings
-
-
-
-
-
 
